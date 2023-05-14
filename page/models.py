db615cd73224ac13d63fa7625f34fe1da3055d11
fourfridays <umair.abbasi@fourfridays.com>
2023-05-14 15:30:52 -0400

    Revert "black formatter"
    This reverts commit 110d1493167d4abd9e5a0a2328566143369a1deb.
    
    # Conflicts:
    #	templates/base.html

--- page/models.py
+++ page/models.py
@@ -4,38 +4,19 @@ from wagtail.admin.panels import FieldPanel
 from wagtail.fields import StreamField
 from wagtail.models import Page
 
-from .blocks import (
-    ImageGridBlock,
-    SingleColumnBlock,
-    TwoColumnBlock,
-    ThreeColumnBlock,
-    FourColumnBlock,
-    HeroImageBlock,
-)
+from .blocks import ImageGridBlock, SingleColumnBlock, TwoColumnBlock, ThreeColumnBlock, FourColumnBlock, HeroImageBlock
 
 
 class StandardPage(Page):
-    body = StreamField(
-        [
-            ("hero_image", HeroImageBlock(icon="image")),
-            ("single_column", SingleColumnBlock(group="COLUMNS")),
-            ("two_columns", TwoColumnBlock(group="COLUMNS")),
-            ("three_columns", ThreeColumnBlock(group="COLUMNS")),
-            ("four_columns", FourColumnBlock(group="COLUMNS")),
-            (
-                "image_grid",
-                ImageGridBlock(
-                    icon="image",
-                    min_num=2,
-                    max_num=4,
-                    help_text="Minimum 2 blocks and a maximum of 4 blocks",
-                ),
-            ),
-        ],
-        use_json_field=True,
-        default="",
-    )
+    body = StreamField([
+        ('hero_image', HeroImageBlock(icon='image')),
+        ('single_column', SingleColumnBlock(group='COLUMNS')),
+        ('two_columns', TwoColumnBlock(group='COLUMNS')),
+        ('three_columns', ThreeColumnBlock(group='COLUMNS')),
+        ('four_columns', FourColumnBlock(group='COLUMNS')),
+        ('image_grid', ImageGridBlock(icon='image', min_num=2, max_num=4, help_text='Minimum 2 blocks and a maximum of 4 blocks')),
+    ], use_json_field=True, default='')
 
     content_panels = Page.content_panels + [
-        FieldPanel("body"),
+        FieldPanel('body'),
     ]
--- page/templatetags/navigation_tags.py
+++ page/templatetags/navigation_tags.py
@@ -13,7 +13,7 @@ def get_site_root(context):
     # This returns a core.Page. The main menu needs to have the site.root_page
     # defined else will return an object attribute error ('str' object has no
     # attribute 'get_children')
-    return Site.find_for_request(context["request"]).root_page
+    return Site.find_for_request(context['request']).root_page
 
 
 def has_menu_children(page):
@@ -30,13 +30,13 @@ def has_children(page):
 
 def is_active(page, current_page):
     # To give us active state on main navigation
-    return current_page.url_path.startswith(page.url_path) if current_page else False
+    return (current_page.url_path.startswith(page.url_path) if current_page else False)
 
 
 # Retrieves the top menu items - the immediate children of the parent page
 # The has_menu_children method is necessary because the Foundation menu requires
 # a dropdown class to be applied to a parent
-@register.inclusion_tag("tags/top_menu.html", takes_context=True)
+@register.inclusion_tag('tags/top_menu.html', takes_context=True)
 def top_menu(context, parent, calling_page=None):
     menuitems = parent.get_children().live().in_menu()
     for menuitem in menuitems:
@@ -44,21 +44,18 @@ def top_menu(context, parent, calling_page=None):
         # We don't directly check if calling_page is None since the template
         # engine can pass an empty string to calling_page
         # if the variable passed as calling_page does not exist.
-        menuitem.active = (
-            calling_page.url_path.startswith(menuitem.url_path)
-            if calling_page
-            else False
-        )
+        menuitem.active = (calling_page.url_path.startswith(menuitem.url_path)
+                           if calling_page else False)
     return {
-        "calling_page": calling_page,
-        "menuitems": menuitems,
+        'calling_page': calling_page,
+        'menuitems': menuitems,
         # required by the pageurl tag that we want to use within this template
-        "request": context["request"],
+        'request': context['request'],
     }
 
 
 # Retrieves the children of the top menu items for the drop downs
-@register.inclusion_tag("tags/top_menu_children.html", takes_context=True)
+@register.inclusion_tag('tags/top_menu_children.html', takes_context=True)
 def top_menu_children(context, parent, calling_page=None):
     menuitems_children = parent.get_children()
     menuitems_children = menuitems_children.live().in_menu()
@@ -67,15 +64,12 @@ def top_menu_children(context, parent, calling_page=None):
         # We don't directly check if calling_page is None since the template
         # engine can pass an empty string to calling_page
         # if the variable passed as calling_page does not exist.
-        menuitem.active = (
-            calling_page.url_path.startswith(menuitem.url_path)
-            if calling_page
-            else False
-        )
+        menuitem.active = (calling_page.url_path.startswith(menuitem.url_path)
+                           if calling_page else False)
         menuitem.children = menuitem.get_children().live().in_menu()
     return {
-        "parent": parent,
-        "menuitems_children": menuitems_children,
+        'parent': parent,
+        'menuitems_children': menuitems_children,
         # required by the pageurl tag that we want to use within this template
-        "request": context["request"],
+        'request': context['request'],
     }
