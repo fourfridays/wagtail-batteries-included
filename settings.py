13ee979474499be86e9cb6aa744a98ad4a6cd9fb
fourfridays <umair.abbasi@fourfridays.com>
2023-05-24 15:50:52 -0400

    set ACL param for S3

--- settings.py
+++ settings.py
@@ -106,6 +106,7 @@ AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME", default=None
 AWS_S3_OBJECT_PARAMETERS = {
     "Expires": "Thu, 31 Dec 2099 20:00:00 GMT",
     "CacheControl": "max-age=94608000",
+    "ACL": "public-read",
 }
 AWS_S3_REGION_NAME = os.environ.get("AWS_S3_REGION_NAME", default=None)
 AWS_S3_ENDPOINT_URL = os.environ.get("AWS_S3_ENDPOINT_URL", default=None)
