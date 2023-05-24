00d6a8b0a6143bf44a9162e4b3edf14a2bb68d99
fourfridays <umair.abbasi@fourfridays.com>
2023-05-24 15:50:34 -0400

    added default none to AWS-S3 settings

--- settings.py
+++ settings.py
@@ -100,15 +100,15 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
+AWS_S3_ACCESS_KEY_ID = os.environ.get("AWS_S3_ACCESS_KEY_ID", default=None)
+AWS_S3_SECRET_ACCESS_KEY = os.environ.get("AWS_S3_SECRET_ACCESS_KEY", default=None)
+AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME", default=None)
 AWS_S3_OBJECT_PARAMETERS = {
     "Expires": "Thu, 31 Dec 2099 20:00:00 GMT",
     "CacheControl": "max-age=94608000",
 }
-AWS_S3_ACCESS_KEY_ID = os.environ.get("AWS_S3_ACCESS_KEY_ID")
-AWS_S3_SECRET_ACCESS_KEY = os.environ.get("AWS_S3_SECRET_ACCESS_KEY")
-AWS_S3_REGION_NAME = os.environ.get("AWS_S3_REGION_NAME")
-AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
-AWS_S3_ENDPOINT_URL = os.environ.get("AWS_S3_ENDPOINT_URL")
+AWS_S3_REGION_NAME = os.environ.get("AWS_S3_REGION_NAME", default=None)
+AWS_S3_ENDPOINT_URL = os.environ.get("AWS_S3_ENDPOINT_URL", default=None)
 
 
 # Static files (CSS, JavaScript, Images)
